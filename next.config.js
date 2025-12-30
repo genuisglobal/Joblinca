39324361ba550a6b3f1895e2c5c19e2c681fbc53
Fidelis Fongum <datagenuisacademy@gmail.com>
2025-12-30 14:59:31 -0800

    fix: remove deprecated experimental.appDir

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
   i18n: {
     // Support both English and French. English is default.
     locales: ['en', 'fr'],
-    defaultLocale: 'en'
-  }
+    defaultLocale: 'en',
+  },
 };
 
 module.exports = nextConfig;
